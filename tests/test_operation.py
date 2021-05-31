b097564bbc60a37f958154c000b5a0ccababd2f1
Pluto Feather <plutofeather@icloud.com>
2021-05-31 12:23:48 -0700

    pylint test

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -4,9 +4,19 @@ import pytest
 import util
 
 
-def test_immediate_operation(cWant, chain,
-                             accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, delegatedVault
-                             ):
+def test_immediate_operation(
+    cWant,
+    chain,
+    accounts,
+    token,
+    vault,
+    strategy,
+    user,
+    strategist,
+    amount,
+    RELATIVE_APPROX,
+    delegatedVault,
+):
     # Deposit to the vault
     user_balance_before = token.balanceOf(user)
     strategy.setBorrowLimit(1000 * 10 ** 18)
@@ -26,14 +36,24 @@ def test_immediate_operation(cWant, chain,
     # withdrawal
     vault.withdraw({"from": user})
     user_balance_after = token.balanceOf(user)
-    assert (pytest.approx(user_balance_after, rel=RELATIVE_APPROX) == user_balance_before)
+    assert pytest.approx(user_balance_after, rel=RELATIVE_APPROX) == user_balance_before
     print("loss: ", (user_balance_before - user_balance_after) / 1e18)
     assert strategy.tendTrigger(0) == False
 
 
-def test_operation(cWant, chain,
-                   accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, delegatedVault
-                   ):
+def test_operation(
+    cWant,
+    chain,
+    accounts,
+    token,
+    vault,
+    strategy,
+    user,
+    strategist,
+    amount,
+    RELATIVE_APPROX,
+    delegatedVault,
+):
     # Deposit to the vault
     user_balance_before = token.balanceOf(user)
     token.approve(vault.address, amount, {"from": user})
@@ -55,18 +75,20 @@ def test_operation(cWant, chain,
 
     # withdrawal
     vault.withdraw({"from": user})
-    assert (pytest.approx(token.balanceOf(user), rel=RELATIVE_APPROX) == user_balance_before)
+    assert (
+        pytest.approx(token.balanceOf(user), rel=RELATIVE_APPROX) == user_balance_before
+    )
     assert strategy.tendTrigger(0) == False
 
 
 def test_emergency_exit(
-        accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, chain
+    accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, chain
 ):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
     vault.deposit(amount, {"from": user})
     strategy.setBorrowLimit(100 * 1e18)
-    print(f'vault pps: {vault.pricePerShare() / 1e18}')
+    print(f"vault pps: {vault.pricePerShare() / 1e18}")
     strategy.harvest()
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
     util.stateOfVault(vault, strategy, token)
@@ -76,12 +98,12 @@ def test_emergency_exit(
 
     # set emergency and exit
     strategy.setEmergencyExit({"from": strategist})
-    print(f'\n before harvest')
-    print(f'vault pps: {vault.pricePerShare()}')
+    print(f"\n before harvest")
+    print(f"vault pps: {vault.pricePerShare()}")
     util.stateOfStrat(strategy, token)
     strategy.harvest({"from": strategist})
-    print(f'\n after harvest')
-    print(f'vault pps: {vault.pricePerShare()}')
+    print(f"\n after harvest")
+    print(f"vault pps: {vault.pricePerShare()}")
     util.stateOfStrat(strategy, token)
     # dust
     assert strategy.estimatedTotalAssets() < 1e12
@@ -90,8 +112,18 @@ def test_emergency_exit(
 
 
 def test_profitable_harvest(
-        accounts, token, vault, weth, delegatedVault, strategy, user, strategist, weth_whale, amount, RELATIVE_APPROX,
-        chain
+    accounts,
+    token,
+    vault,
+    weth,
+    delegatedVault,
+    strategy,
+    user,
+    strategist,
+    weth_whale,
+    amount,
+    RELATIVE_APPROX,
+    chain,
 ):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
@@ -103,7 +135,9 @@ def test_profitable_harvest(
 
     # Harvest 1: Send funds through the strategy
     strategy.harvest({"from": strategist})
-    assert strategy.valueOfDelegated() > 0  # ensure funds have been deposited into delegated vault
+    assert (
+        strategy.valueOfDelegated() > 0
+    )  # ensure funds have been deposited into delegated vault
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     # increase rewards, lending interest and borrowing interests
@@ -112,7 +146,9 @@ def test_profitable_harvest(
     chain.mine(1)
 
     strategy.harvest()
-    weth.transfer(delegatedVault, Wei("20_000 ether"), {"from": weth_whale})  # simulate delegated vault interest
+    weth.transfer(
+        delegatedVault, Wei("20_000 ether"), {"from": weth_whale}
+    )  # simulate delegated vault interest
 
     # Harvest 2: Realize profit
     before_pps = vault.pricePerShare()
@@ -130,11 +166,24 @@ def test_profitable_harvest(
     assert vault.totalAssets() > amount
 
 
-def test_profitable_harvest_with_collateral_injection(accounts, token, vault, weth, delegatedVault, strategy, user,
-                                                      strategist,
-                                                      weth_whale, amount, RELATIVE_APPROX,
-                                                      chain,
-                                                      cSupplied, cSupplied_whale, inverseGov, cSupply_amount):
+def test_profitable_harvest_with_collateral_injection(
+    accounts,
+    token,
+    vault,
+    weth,
+    delegatedVault,
+    strategy,
+    user,
+    strategist,
+    weth_whale,
+    amount,
+    RELATIVE_APPROX,
+    chain,
+    cSupplied,
+    cSupplied_whale,
+    inverseGov,
+    cSupply_amount,
+):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
     vault.deposit(amount, {"from": user})
@@ -145,25 +194,29 @@ def test_profitable_harvest_with_collateral_injection(accounts, token, vault, we
 
     # Harvest 1: Send funds through the strategy
     strategy.harvest({"from": strategist})
-    assert strategy.valueOfDelegated() > 0  # ensure funds have been deposited into delegated vault
+    assert (
+        strategy.valueOfDelegated() > 0
+    )  # ensure funds have been deposited into delegated vault
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     cSupplied.approve(strategy, 2 ** 256 - 1, {"from": inverseGov})
 
-    print('before injection')
+    print("before injection")
     util.stateOfStrat(strategy, token)
     strategy.supplyCollateral(cSupply_amount, {"from": inverseGov})
 
     assert strategy.valueOfCSupplied() > 0
 
-    print('after injection')
+    print("after injection")
     util.stateOfStrat(strategy, token)
     # increase rewards, lending interest and borrowing interests
     # assets_before = vault.totalAssets()
     chain.sleep(30 * 24 * 3600)  # 30 days
     chain.mine(1)
     strategy.harvest()
-    weth.transfer(delegatedVault, Wei("20 ether"), {"from": weth_whale})  # simulate delegated vault interest
+    weth.transfer(
+        delegatedVault, Wei("20 ether"), {"from": weth_whale}
+    )  # simulate delegated vault interest
 
     # Harvest 2: Realize profit
     before_pps = vault.pricePerShare()
@@ -179,18 +232,18 @@ def test_profitable_harvest_with_collateral_injection(accounts, token, vault, we
     assert strategy.estimatedTotalAssets() + profit > amount
     assert vault.pricePerShare() > before_pps
     assert vault.totalAssets() > amount
-    print('before removed')
+    print("before removed")
     util.stateOfStrat(strategy, token)
 
     strategy.removeCollateral(cSupply_amount, {"from": inverseGov})
 
-    print('after removed')
+    print("after removed")
     util.stateOfStrat(strategy, token)
     assert cSupplied.balanceOf(inverseGov) == cSupply_amount
 
 
 def test_change_debt(
-        gov, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
+    gov, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
 ):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": user})
@@ -199,31 +252,41 @@ def test_change_debt(
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.harvest()
     half = int(amount / 2)
-    print('debtRatio 5000')
+    print("debtRatio 5000")
     util.stateOfStrat(strategy, token)
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == half
 
     vault.updateStrategyDebtRatio(strategy.address, 10_000, {"from": gov})
     strategy.harvest()
 
-    print('debtRatio 10000')
+    print("debtRatio 10000")
     util.stateOfStrat(strategy, token)
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.harvest()
 
-    print('debtRatio 5000')
+    print("debtRatio 5000")
     util.stateOfStrat(strategy, token)
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == half
 
 
 def test_change_debt_with_injection(
-        gov, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, cSupplied, cSupply_amount, inverseGov
+    gov,
+    token,
+    vault,
+    strategy,
+    user,
+    strategist,
+    amount,
+    RELATIVE_APPROX,
+    cSupplied,
+    cSupply_amount,
+    inverseGov,
 ):
     # Inject cSupplied
     cSupplied.approve(strategy, 2 ** 256 - 1, {"from": inverseGov})
-    print('before injection')
+    print("before injection")
     util.stateOfStrat(strategy, token)
     strategy.supplyCollateral(cSupply_amount, {"from": inverseGov})
 
@@ -234,7 +297,7 @@ def test_change_debt_with_injection(
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.harvest()
 
-    print('debtRatio 5000')
+    print("debtRatio 5000")
     util.stateOfStrat(strategy, token)
 
     half = int(amount / 2)
@@ -244,7 +307,7 @@ def test_change_debt_with_injection(
     vault.updateStrategyDebtRatio(strategy.address, 10_000, {"from": gov})
     strategy.harvest()
 
-    print('debtRatio 10000')
+    print("debtRatio 10000")
     util.stateOfStrat(strategy, token)
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
@@ -252,12 +315,25 @@ def test_change_debt_with_injection(
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.harvest()
 
-    print('debtRatio 5000')
+    print("debtRatio 5000")
     util.stateOfStrat(strategy, token)
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == half
 
 
-def test_collateral_factor(token, vault, cBorrowed, strategy, user, strategist, amount, RELATIVE_APPROX, weth, weth_whale, delegatedVault, chain):
+def test_collateral_factor(
+    token,
+    vault,
+    cBorrowed,
+    strategy,
+    user,
+    strategist,
+    amount,
+    RELATIVE_APPROX,
+    weth,
+    weth_whale,
+    delegatedVault,
+    chain,
+):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
     strategy.setBorrowLimit(1000 * 1e18, {"from": strategist})
@@ -270,18 +346,18 @@ def test_collateral_factor(token, vault, cBorrowed, strategy, user, strategist, 
     util.stateOfVault(vault, strategy, token)
 
     with brownie.reverts("too low"):
-        strategy.setTargetCollateralFactor(.01 * 1e18)
+        strategy.setTargetCollateralFactor(0.01 * 1e18)
     with brownie.reverts("too high"):
-        strategy.setTargetCollateralFactor(.6 * 1e18)
+        strategy.setTargetCollateralFactor(0.6 * 1e18)
 
-    strategy.setTargetCollateralFactor(.1 * 1e18)
+    strategy.setTargetCollateralFactor(0.1 * 1e18)
     assert strategy.harvestTrigger(0) == False
     assert strategy.tendTrigger(0) == True
     strategy.tend({"from": strategist})
     util.stateOfStrat(strategy, token)
     util.stateOfVault(vault, strategy, token)
 
-    strategy.setTargetCollateralFactor(.5 * 1e18)
+    strategy.setTargetCollateralFactor(0.5 * 1e18)
     assert strategy.harvestTrigger(0) == False
     assert strategy.tendTrigger(0) == True
     strategy.tend({"from": strategist})
@@ -289,7 +365,9 @@ def test_collateral_factor(token, vault, cBorrowed, strategy, user, strategist, 
     util.stateOfVault(vault, strategy, token)
 
     # give it some profits
-    weth.transfer(delegatedVault, Wei("20_000 ether"), {"from": weth_whale})  # simulate delegated vault interest
+    weth.transfer(
+        delegatedVault, Wei("20_000 ether"), {"from": weth_whale}
+    )  # simulate delegated vault interest
     assert strategy.harvestTrigger(0) == True
     assert strategy.tendTrigger(0) == False
     strategy.harvest()
@@ -299,7 +377,7 @@ def test_collateral_factor(token, vault, cBorrowed, strategy, user, strategist, 
     util.stateOfStrat(strategy, token)
     util.stateOfVault(vault, strategy, token)
 
-    strategy.setTargetCollateralFactor(.1 * 1e18)
+    strategy.setTargetCollateralFactor(0.1 * 1e18)
     assert strategy.harvestTrigger(0) == True
     assert strategy.tendTrigger(0) == False
     strategy.harvest({"from": strategist})
@@ -307,7 +385,9 @@ def test_collateral_factor(token, vault, cBorrowed, strategy, user, strategist, 
     util.stateOfVault(vault, strategy, token)
 
 
-def test_borrow_limit(token, vault, cBorrowed, strategy, user, strategist, amount, RELATIVE_APPROX):
+def test_borrow_limit(
+    token, vault, cBorrowed, strategy, user, strategist, amount, RELATIVE_APPROX
+):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
     vault.deposit(amount, {"from": user})
@@ -331,15 +411,24 @@ def test_borrow_limit(token, vault, cBorrowed, strategy, user, strategist, amoun
     borrowed_value = strategy.valueOfBorrowedOwed()
     assert borrowed_amount > 0
     assert borrowed_value > 0
-    assert pytest.approx(strategy.valueOfDelegated(), rel=RELATIVE_APPROX) == borrowed_value
+    assert (
+        pytest.approx(strategy.valueOfDelegated(), rel=RELATIVE_APPROX)
+        == borrowed_value
+    )
 
     # reduce borrow limit
     half_borrowed_amount = borrowed_amount / 2
     half_borrowed_value = borrowed_value / 2
     strategy.setBorrowLimit(half_borrowed_amount, {"from": strategist})
     strategy.tend({"from": strategist})
-    assert pytest.approx(cBorrowed.borrowBalanceStored(strategy), rel=RELATIVE_APPROX) == half_borrowed_amount
-    assert pytest.approx(strategy.valueOfBorrowedOwed(), rel=RELATIVE_APPROX) == half_borrowed_value
+    assert (
+        pytest.approx(cBorrowed.borrowBalanceStored(strategy), rel=RELATIVE_APPROX)
+        == half_borrowed_amount
+    )
+    assert (
+        pytest.approx(strategy.valueOfBorrowedOwed(), rel=RELATIVE_APPROX)
+        == half_borrowed_value
+    )
 
     # disable borrowing
     strategy.setBorrowLimit(0, {"from": strategist})
@@ -349,7 +438,9 @@ def test_borrow_limit(token, vault, cBorrowed, strategy, user, strategist, amoun
     assert strategy.valueOfBorrowedOwed() == 0
 
 
-def test_sweep(gov, vault, strategy, token, user, amount, inv, inv_whale, rook, rook_whale):
+def test_sweep(
+    gov, vault, strategy, token, user, amount, inv, inv_whale, rook, rook_whale
+):
     # Strategy want token doesn't work
     token.transfer(strategy, amount, {"from": user})
     assert token.address == strategy.want()
@@ -385,7 +476,7 @@ def test_sweep(gov, vault, strategy, token, user, amount, inv, inv_whale, rook, 
 
 
 def test_triggers(
-        gov, vault, strategy, token, amount, user, weth, weth_amout, strategist
+    gov, vault, strategy, token, amount, user, weth, weth_amout, strategist
 ):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": user})
